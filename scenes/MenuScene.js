becfefa7d052bbb3a07f17d9ca820de4c181eec5
hungnt <nthung90@yahoo.com>
2019-02-27 13:05:32 +0700

    Create HowToPlay

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -2,11 +2,14 @@ import Config from './Config'
 var app
 class MenuScene extends Phaser.Scene {
     constructor() {
-        super({ key: 'MenuScene' })
+        super({
+            key: 'MenuScene'
+        })
     }
-
     preload() {
         this.load.image('play', 'assets/images/play.png')
+        this.load.image('howtoplay', 'assets/images/HowToPlay.png')
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
     }
     create() {
         app = this
@@ -20,9 +23,26 @@ class MenuScene extends Phaser.Scene {
             repeat: -1,
             yoyo: true
         })
-        var ting = this.add.text(Config.BG_WIDTH / 2, 200, 'TING', { font: '128px Courier bold', fill: '#4286f4', align: 'center' }).setOrigin(0.5, 0.5)
 
-        var playBtn = this.add.image(Config.BG_WIDTH / 2, Config.BG_HEIGHT / 2, 'play').setOrigin(0.5, 0.5).setInteractive()
+        // Game title
+        // var ting = this.add.text(Config.BG_WIDTH / 2, 50, 'Zombie pin ball', { font: '64px Courier bold', fill: '#4286f4', align: 'center' }).setOrigin(0.5, 0.5)
+        var add = this.add
+        WebFont.load({
+            google: {
+                families: ['Freckle Face']
+            },
+            active: function() {
+                add.text(Config.BG_WIDTH / 2, 50, 'Zombie pin ball', {
+                    fontFamily: 'Freckle Face',
+                    fontSize: 80,
+                    color: '#ffffff'
+                }).setShadow(2, 2, "#333333", 2, false, true).setOrigin(0.5, 0.5)
+            }
+        })
+
+        this.add.image(Config.BG_WIDTH / 2, 350, 'howtoplay').setOrigin(0.5, 0.5)
+
+        var playBtn = this.add.image(Config.BG_WIDTH / 2, 700, 'play').setOrigin(0.5, 0.5).setInteractive()
         playBtn.on('pointerdown', () => this.scene.start("GameScene"))
     }
 }
